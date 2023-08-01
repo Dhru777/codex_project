139ef74ab4e41f8e31591e0193c3e820d7d030b3
Dhruthi <dhruthi.chennupati@gmail.com>
2023-08-01 23:52:30 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -95,7 +95,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from the server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-project-13p4.onrender.com', {
       method : 'POST',
       headers: {
         'Content-Type' : 'application/json'
